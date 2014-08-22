d2be687ab52a95018e8a56cbb19c460aba4c2182
Jason Brooks <jbrooks@redhat.com>
2014-08-22 13:35:17 -0700

    temp hack to work around docker issue 6137, currently present in centos docker pkg

--- postgres/start_postgres.sh
+++ postgres/start_postgres.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# sort of a temp hack to make this work on docker pkg in centos7
+# due to https://github.com/docker/docker/issues/6137
+
+chmod 0700 /var/lib/pgsql/data
+
 __run_supervisor() {
 supervisord -n
 }
